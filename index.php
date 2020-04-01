f976b689e73533775b4ef48d5435f1de20b8181a
brendapc <brendacamara@outlook.com>
2020-04-01 11:01:32 -0300

    form ok

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 <head>
     <meta charset="UTF-8">
-    
     <title>inscrição</title>
 </head>
 <body>
     <p>Formulário para inscrição dos competidores</p>
-    <form action="script.php" method="post"></form>
+<form action="script.php" method="post">
     <p>seu nome <input type="text" name="nome"/></p>
     <p>sua idade <input type="text" name="idade"/></p>
-    <p><input type="submit"/></p>
+    <p><input type="submit" value="enviar dados"/></p>
+</form>
 </body>
 </html>
--- script.php
+++ script.php
@@ -5,8 +5,32 @@ $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 $categorias[] = 'idoso';
 
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
+$nome = $_POST["nome"];
+$idade = $_POST["idade"];
 
-var_dump($idade);
-var_dump($nome);
+if($idade >=6 && $idade<12){
+    for($i= 0; $i< count($categorias); $i++){
+        if($categorias[$i] == 'infantil'){
+            echo 'o nadador '. $nome . ' compete na categoria '. $categorias[$i];
+        }
+    }
+}else if($idade >=13 && $idade <= 18){
+    for($i= 0; $i< count($categorias); $i++){
+        if($categorias[$i] == 'adoclescente'){
+            echo 'o nadador '. $nome . ' compete na categoria '. $categorias[$i]; 
+        }
+    }
+}else if($idade >=18 && $idade < 60){
+    for($i= 0; $i< count($categorias); $i++){
+        if($categorias[$i] == 'adulto'){
+            echo 'o nadador '. $nome . ' compete na categoria '. $categorias[$i]; 
+        }
+    }
+}else{
+    for($i= 0; $i< count($categorias); $i++){
+        if($categorias[$i] == 'idoso'){
+            echo 'o nadador '. $nome . ' compete na categoria '. $categorias[$i];
+        }
+    }
+}
+?>
