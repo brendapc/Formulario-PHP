2327535191e5771bf02ba4fcf1a2ba4b8b57d312
brendapc <brendacamara@outlook.com>
2020-04-01 10:31:04 -0300

    new script

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>inscrição</title>
+</head>
+<body>
+    <p>Formulário para inscrição dos competidores</p>
+    <form action="script.php" method="post"></form>
+    <p>seu nome <input type="text" name="nome"/></p>
+    <p>sua idade <input type="text" name="idade"/></p>
+    <p><input type="submit"/></p>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+
+$nome = $_POST[]
+$idade = $_
